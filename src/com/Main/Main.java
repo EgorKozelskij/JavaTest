dac89332989adf894e996fd3a344f193962b8e06
Egor <jegorkozelskij@gmail.com>
2016-07-20 16:26:32 +0300

    insert 'git'
    a
    a
    a
    df
    :

--- src/com/Main/Main.java
+++ src/com/Main/Main.java
@@ -1,7 +1,14 @@
 package com.Main;
 
+import java.util.HashMap;
+
 public class Main
 {
+
+    static {
+        System.out.print("Way");
+    }
+
     public static void main(String[] args)
     {
         System.out.println("Hi my World! Вот вот");
